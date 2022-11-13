37a26ffcf0105d35cf3bd2e3dda6698a9b019c5f
Nektarinchik <ermolen2003@gmail.com>
2022-11-13 16:19:39 +0300

    add components validation

--- WebAutopark/WebAutopark.DAL/Entities/Components.cs
+++ WebAutopark/WebAutopark.DAL/Entities/Components.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace WebAutopark.DAL.Entities
 {
     public sealed class Components
     {
         public int ComponentId { get; set; }
+
+        [Required(ErrorMessage = "This is required field!")]
         public string Name { get; set; } = null!;
         public Components() 
         { }
